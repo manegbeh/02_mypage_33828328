b063be186bcbdb3aa643a73469aa367b82c94e51
manegbeh <150786806+manegbeh@users.noreply.github.com>
2025-10-09 17:37:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,18 +13,8 @@ const server = http.createServer((req, res) => {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>About Me</title>
-        <style>
-          h1 {
-            max-width: 600px;
-            padding: 20px;
-            background: #ffffff;
-            border: 2px solid #ddd;
-            border-radius: 10px;
-            box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
-            font-size: 1.1rem;
-          }  
-            
-          p {
+        <style> 
+          body {
             max-width: 600px;
             padding: 20px;
             background: #ffffff;
